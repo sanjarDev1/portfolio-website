10c65658c6d2c8cfb83d820c42e1c9f8097ed2a1
sanjarbek <sanjarbekqobulov02@gmail.com>
2022-04-27 23:53:00 +0500

    task

--- src/home/AboutInfo.js
+++ src/home/AboutInfo.js
@@ -45,7 +45,7 @@ function AboutInfo() {
               <Link className="linkBtn" to="/">
                 About me
               </Link>
-                <a className="linkBtn" href={item.resume}   download   ><FileDownloadIcon /> Portfolio</a>
+                <a className="linkBtn" href={item.resume}   download  target={'_blank'}  ><FileDownloadIcon /> Portfolio</a>
             </div>
             <div className="socialNet">
               <a className="links" href="https://github.com/sanjarDev1"  >
